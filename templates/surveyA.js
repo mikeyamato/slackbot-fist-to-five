3e3ea9036e9dd88369d9f661b95fe9f28db57be4
Mix Master Mike <mikeyamato@gmail.com>
2018-09-19 13:59:52 -0700

    testing surveyA.js

--- templates/surveyA.js
+++ templates/surveyA.js
@@ -5,8 +5,11 @@ module.exports = {
 				"title": "Network traffic (kb/s)",
 				"title_link": "https://datadog.com/path/to/event",
 				"text": "How does this look? @slack-ops - Sent by Julie Dodd",
-				"image_url": "https://datadoghq.com/snapshot/path/to/snapshot.png",
+				"image_url": "https://www.youtube.com/embed/hlH9RGLJqxE",
 				"color": "#764FA5"
 		}
 	]
 }
+
+
+{/* <iframe width="560" height="315" src="https://www.youtube.com/embed/hlH9RGLJqxE" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe> */}
