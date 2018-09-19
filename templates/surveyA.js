9355d6d164bb2083ce9cf2d960add03fef62f9c8
Mix Master Mike <mikeyamato@gmail.com>
2018-09-19 14:58:12 -0700

    logging results

--- templates/surveyA.js
+++ templates/surveyA.js
@@ -5,18 +5,18 @@ module.exports = {
 				"title": "Thanks for casting your vote",
 				"title_link": "https://www.google.com",
 				"text": "What does this look like?",
-				"fields": [
-					{
-							"title": "Some",
-							"value": "Example",
-							"short": true
-					},
-					{
-						"title": "Another",
-						"value": "Greate Example",
-						"short": true
-					}
-				],
+				// "fields": [
+				// 	{
+				// 			"title": "Some",
+				// 			"value": "Example",
+				// 			"short": true
+				// 	},
+				// 	{
+				// 		"title": "Another",
+				// 		"value": "Greate Example",
+				// 		"short": true
+				// 	}
+				// ],
 				"image-url": "https://media.giphy.com/media/Vccpm1O9gV1g4/giphy.gif",
 				"color": "#764FA5",
 		}
