f9d2d59b1281b7637cd980a8813309c59d1a433a
Mix Master Mike <mikeyamato@gmail.com>
2018-09-18 23:21:46 -0700

    updated slashCommand.js

--- routes/api/slashCommand.js
+++ routes/api/slashCommand.js
@@ -8,23 +8,21 @@ const foodEmoji = require('../../assets/foodEmoji');
 // post request
 router.post('/', (req, res) => {
 	const singleFoodEmoji = foodEmoji[Math.floor(Math.random() * foodEmoji.length)];
+	const requestType = req.body.command;
 
-
-
-	// const requestType = req.body.text;
 	// console.log('**** 1', req)
 	console.log('**** 2', req.body);
 	// console.log('**** 3', requestType);
 
-	// if(req.body){
-	// 	res.status(200).send(
-	// 		surveyQ `\n${singleFoodEmoji}`
-	// )} else {
-	// 	res.status(200).send(
-	// 	{
-	// 		"text": `Zoinks! \nSomething doesn't look right. \nPlease try again. \n${singleFoodEmoji}`
-	// 	}
-	// )}
+	if(requestType === '/fist'){
+		res.status(200).send(
+			surveyQ `\n${singleFoodEmoji}`
+	)} else {
+		res.status(200).send(
+		{
+			"text": `Zoinks! \nSomething doesn't look right. \nPlease try again. \n${singleFoodEmoji}`
+		}
+	)}
 })
 
 module.exports = router;
