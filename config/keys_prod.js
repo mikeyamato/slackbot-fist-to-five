cdec4b83f1139be10d561b02688410bcaeb526b7
Mix Master Mike <mikeyamato@gmail.com>
2018-09-24 10:35:31 -0700

    removed unneeded tokens

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,6 +1,4 @@
 module.exports = {
 	slackTokenBotTonkotsu: process.env.TONKOTSU_BOT_SLACK_TOKEN,
-	slackTokenWorkspaceTonkotsu: process.env.TONKOTSU_WORKSPACE_SLACK_TOKEN,
-	slackTokenBotUclaBootcamp: process.env.UCLA_BOOTCAMP_BOT_SLACK_TOKEN,
-	tonkotsuSlackWorkspaceSigningSecret: process.env.TONKOTSU_SLACK_WORKSPACE_SIGNING_SECRET
+	slackTokenBotUclaBootcamp: process.env.UCLA_BOOTCAMP_BOT_SLACK_TOKEN
 };
