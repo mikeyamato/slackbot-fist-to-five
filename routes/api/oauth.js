59e8ae095f7836836028539f55b435f7dc6ceac6
Mix Master Mike <mikeyamato@gmail.com>
2018-09-23 22:24:03 -0700

    updated oauth.js

--- routes/api/oauth.js
+++ routes/api/oauth.js
@@ -2,12 +2,24 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', (req, res) => {
-	console.log('#### req.body', req.body);
-	res.status(200).send(
-		{
-			"text": req.body.challenge
-		}
-	)
+	let payload = req.body;
+	res.sendStatus(200);
+
+	if (payload.event.type === "message") {
+			let response_text;
+			if (payload.event.text.includes("<@UBJTTTDRT>") && payload.event.text.includes("tell me a joke")) {
+					response_text = `Hello <@${payload.event.username}>! Knock, knock.`
+			}
+			if (payload.event.text.includes("Who's there?")) {
+					response_text = "A bot user";
+			}
+			if (payload.event.text.includes("Bot user who?")) {
+					response_text = "No, I'm a bot user. I don't understand jokes.";
+			}
+			if (response_text !== undefined) {
+					// Make call to chat.postMessage sending response_text using bot's token
+			}
+	}
 } )
 
 
