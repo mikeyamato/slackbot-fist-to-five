e7a6c5bd37806ef0832877f71a591c8d143e3876
Mix Master Mike <mikeyamato@gmail.com>
2018-09-23 22:28:37 -0700

    updated oauth.js

--- routes/api/oauth.js
+++ routes/api/oauth.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/', (req, res, next) => {
 	let payload = req.body;
 	res.sendStatus(200);
+	console.log('#### payload', payload);
 
 	if (payload.event.type === "message") {
 			let response_text;
