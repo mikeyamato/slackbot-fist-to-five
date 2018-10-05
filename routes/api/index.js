a8c54fced49ef9151b2dae6c44b71f7df978e8a9
Mix Master Mike <mikeyamato@gmail.com>
2018-10-05 00:28:43 -0700

    adjusted index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -72,14 +72,92 @@ router.post('/', (req, res) => {
 
 		// send survey out
 		res.status(200).send(
-			postSurvey()
-		)} else {
-			res.status(200).send(
-		{
-			"text": `Zoinks! \nSomething doesn't look right. \nPlease try again. \n${singleFoodEmoji}`
-		}
-	)}
+			// surveyQ
+			surveyToClass()
+		)
+	} else {
+		res.status(200).send(
+			{
+				"text": `Zoinks! \nSomething doesn't look right. \nPlease try again. \n${singleFoodEmoji}`
+			}
+		)
+	}
 })
+				
+/************************************************/
+
+function surveyToClass() {
+
+	const postMessage	= 'https://slack.com/api/chat.postMessage';
+	const channelPortion = `&channel=${channelId}`;  
+	const textPortion = '&text=What time is it? It\'s Fist-to-Five survey time! Yay! :tada:';
+	const attachmentsPortion = '&attachments='+encodeURIComponent([{
+		"title": "How well do you understand this material? \n \n As always, responses are 100% anonymous.\n",
+		"callback_id": "fist_results",
+		"attachment_type": "default",
+		"color": "#FF9DBB",
+		"actions": [
+			{
+				"name": "fist_select",
+				"text": "Select one...",
+				"type": "select",
+				"options": [
+					{
+						"text": "Fist  (Help, I'm lost)",
+						"value": "fist"
+					},
+					{
+						"text": "1  (I barely understand)",
+						"value": "one_finger"
+					},
+					{
+						"text": "2  (I'm starting to understand)",
+						"value": "two_fingers"
+					},
+					{
+						"text": "3  (I somewhat get it)",
+						"value": "three_fingers"
+					},
+					{
+						"text": "4  (I'm comfortable with the idea)",
+						"value": "four_fingers"
+					},
+					{
+						"text": "5  (I understand this 100%)",
+						"value": "five_fingers"
+					},
+				],
+				"confirm": {
+					"title": "Are you sure?",
+					"text": "Just confirming your selection. :nerd_face:",
+					"ok_text": "Yes, I'm sure",
+					"dismiss_text": "No, I'm not sure"
+				}
+			}
+		]
+	}]);
+	const prettyPortion = '&pretty=1';  // no documentation availble about what this does
+
+	const postSurveyResults = {
+		url: postMessage+slackTokenPortion+channelPortion+textPortion+attachmentsPortion+prettyPortion,
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json; charset=utf-8',
+		}
+	}
+
+	request(postSurveyResults, function (error, response) {
+		
+		console.log('##############initial# error', error);
+		
+		return;
+	});
+}
+
+
+
+/************************************************/
+							
 
 
 // posting survey form on slack
