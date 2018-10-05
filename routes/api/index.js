458853293b36678a546f55681b3dfc1ec7d20d24
Mix Master Mike <mikeyamato@gmail.com>
2018-10-05 01:04:35 -0700

    logging results

--- routes/api/index.js
+++ routes/api/index.js
@@ -86,6 +86,8 @@ router.post('/', (req, res) => {
 				
 /************************************************/
 
+const attachmentsPortion = '&attachments='+encodeURIComponent(`[{${surveyQ}}]`);
+
 function surveyToClass() {
 
 	const postMessage	= 'https://slack.com/api/chat.postMessage';
@@ -97,7 +99,7 @@ function surveyToClass() {
 	
 	const channelPortion = `&channel=${channelId}`;  
 	const textPortion = '&text=What time is it? It\'s Fist-to-Five survey time! Yay! :tada:';
-	const attachmentsPortion = '&attachments='+encodeURIComponent(`[{${surveyQ}}]`);
+	// const attachmentsPortion = '&attachments='+encodeURIComponent(`[{${surveyQ}}]`);
 	const prettyPortion = '&pretty=1';  // no documentation availble about what this does
 
 	const postSurveyResults = {
